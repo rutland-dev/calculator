301ccc7cac5b2d673d38ca8f406e397507afb9e6
Joshua Rutland <144169110+rutland-dev@users.noreply.github.com>
2023-10-31 00:55:31 -0400

    Make operate() change the display content
    Fixed the operate function so that the display is updated
    with the new total.

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+let total = 0;
+
 const add = (firstNumber, secondNumber) => {
     return parseInt(firstNumber) + parseInt(secondNumber);
 }
@@ -16,10 +18,27 @@ const divide = (firstNumber, secondNumber) => {
 
 const operate = (firstNumber, secondNumber, operator) => {
     if (operator === '-') {
-        return subtract(firstNumber, secondNumber);
+        total = subtract(firstNumber, secondNumber);
     } else if (operator === '*') {
-        return multiply(firstNumber, secondNumber);
+        total = multiply(firstNumber, secondNumber);
     } else if (operator === '/') {
-        return divide(firstnumber, secondNumber);
-    } else return add(firstNumber, secondNumber);
+        total = divide(firstnumber, secondNumber);
+    } else total = add(firstNumber, secondNumber);
+    display.textContent = total;
 };
+
+const display = document.querySelector('#display-result');
+display.textContent = total;
+
+const checkIfZero = () => (display.textContent == 0) ? true : false;
+
+const clearKey = document.querySelector('#clear');
+clearKey.addEventListener("click", clear);
+function clear() {
+    total += 1;
+    display.textContent = total;
+};
+
+const plusMinus = document.querySelector('#plus-minus');
+plusMinus.addEventListener('click', () => operate(parseInt(display.textContent), -1, '*'));
+
