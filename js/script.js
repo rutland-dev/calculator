67a74167e970fb7ffff0fa783dfe18fb6d9a330b
Joshua Rutland <144169110+rutland-dev@users.noreply.github.com>
2023-10-31 10:19:24 -0400

    Add code for all number buttons
    Added code for all number buttons.

--- js/script.js
+++ js/script.js
@@ -48,6 +48,7 @@ function clear() {
     display.textContent = total;
 };
 
+// Operator Keys
 const plusMinusButton = document.querySelector('#plus-minus');
 plusMinusButton.addEventListener('click', () => operate(displayText, -1, '*'));
 
@@ -55,16 +56,75 @@ const percentButton = document.querySelector('#percent');
 percentButton.addEventListener('click', () => alert('work in progress'));
 
 const divideButton = document.querySelector('#divide');
-divideButton.addEventListener('click', () => {firstNumberVar = total;
-                                            operatorVar = '/';
-                                            total = '0';
-                                            display.textContent = '0';
-                                        });
+divideButton.addEventListener('click', () => {
+    firstNumberVar = total; 
+    operatorVar = '/'; 
+    total = '0'; 
+    display.textContent = '0';
+});
 
 const equalButton = document.querySelector('#equal');
 equalButton.addEventListener('click', () => operate(firstNumberVar, total, operatorVar));
 
+// Number Keys
+
+
 const oneButton = document.querySelector('#one');
-oneButton.addEventListener('click', () => {checkIfZero() ? total = '1' : total += '1';
-                                        display.textContent = total;
-                                    });
+oneButton.addEventListener('click', () => {
+    checkIfZero() ? total = '1' : total += '1'; 
+    display.textContent = total;
+});
+
+const twoButton = document.querySelector('#two');
+twoButton.addEventListener('click', () => {
+    checkIfZero() ? total = '2' : total += '2'; 
+    display.textContent = total;
+});
+
+const threeButton = document.querySelector('#three');
+threeButton.addEventListener('click', () => {
+    checkIfZero() ? total = '3' : total += '3'; 
+    display.textContent = total;
+});
+
+const fourButton = document.querySelector('#four');
+fourButton.addEventListener('click', () => {
+    checkIfZero() ? total = '4' : total += '4'; 
+    display.textContent = total;
+});
+
+const fiveButton = document.querySelector('#five');
+fiveButton.addEventListener('click', () => {
+    checkIfZero() ? total = '5' : total += '5'; 
+    display.textContent = total;
+});
+
+const sixButton = document.querySelector('#six');
+sixButton.addEventListener('click', () => {
+    checkIfZero() ? total = '6' : total += '6'; 
+    display.textContent = total;
+});
+
+const sevenButton = document.querySelector('#seven');
+sevenButton.addEventListener('click', () => {
+    checkIfZero() ? total = '7' : total += '7'; 
+    display.textContent = total;
+});
+
+const eightButton = document.querySelector('#eight');
+eightButton.addEventListener('click', () => {
+    checkIfZero() ? total = '8' : total += '8'; 
+    display.textContent = total;
+});
+
+const nineButton = document.querySelector('#nine');
+nineButton.addEventListener('click', () => {
+    checkIfZero() ? total = '9' : total += '9'; 
+    display.textContent = total;
+});
+
+const zeroButton = document.querySelector('#zero');
+zeroButton.addEventListener('click', () => {
+    checkIfZero() ? total = '0' : total += '0'; 
+    display.textContent = total;
+});
